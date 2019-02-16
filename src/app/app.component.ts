ee91ddd4071a068b110b4d1258e4e527f80583ae
Ivan Danylenko <danylenko.ivan11@gmail.com>
2019-02-16 13:17:31 +0200

    success in getting api population

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ApiPopulationService } from './api-population.service';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bluefield-technologies';
+  settings: object = {
+    base: 'http://api.population.io:80/1.0/population',
+    country: 'Slovak%20Republic',
+    year: '1990',
+  }
+  response: any;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  search() {
+    /* Countries */
+    // this.http.get('http://api.population.io:80/1.0/countries')
+    // .subscribe((response)=>{
+    //   this.response = response;
+    //   console.log(this.response);
+    // });
+
+    /* Current */
+    this.http.get('http://api.population.io:80/1.0/population/1990/Slovak%20Republic/19/')
+    .subscribe((response)=>{
+      this.response = response[0];
+      console.log(this.response);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { Task1Component } from './task1/task1.component';
+import { HttpClient } from 'selenium-webdriver/http';
+import { ApiPopulationService } from './api-population.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Task1Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ApiPopulationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
